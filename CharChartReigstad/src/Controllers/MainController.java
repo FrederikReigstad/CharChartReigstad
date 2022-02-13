9e12c7918cc105d9c3181962048db2495571b005
FrederikReigstad <Frederik9876@gmail.com>
2022-02-13 13:07:24 +0100

    Det er et new view til area chart

--- CharChartReigstad/src/Controllers/MainController.java
+++ CharChartReigstad/src/Controllers/MainController.java
@@ -1,13 +1,22 @@
+package Controllers;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class MainController {
+    @FXML
+    public Button AreaChartBt;
     @FXML
     private LineChart<Number, Number> chartLine;
 
@@ -35,5 +44,13 @@ public class MainController {
 
         chartLine.getData().add(series);
 
+    }
+
+
+    public void AreaChartGoBt(ActionEvent actionEvent) throws IOException {
+        Stage switchScene = (Stage) AreaChartBt.getScene().getWindow();
+        Parent parent = FXMLLoader.load(getClass().getResource("View/AreaChart.fxml"));
+        Scene scene = new Scene(parent);
+        switchScene.setScene(scene);
     }
 }
--- CharChartReigstad/src/Main.java
+++ CharChartReigstad/src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainVeiw.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/MainVeiw.fxml"));
         primaryStage.setTitle("Javafx Charts Exampels");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
